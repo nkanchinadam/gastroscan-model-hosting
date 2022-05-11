2c9a9f8d6cabcf2c2fbc2277e51ca1e9488b45a5
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-05-11 09:29:32 -0400

    created max_probability() to determine the index of the label that the model has classified an image as

--- determine_label.py
+++ determine_label.py
@@ -4,3 +4,12 @@ def get_labels(filepath):
   for line in file:
     labels.append(line.strip())
   return labels
+
+def max_probability(predictions):
+  index = 0
+  max_prob = predictions[0]
+  for i in range(1, len(predictions)):
+    if predictions[i] > max_prob:
+      max_prob = predictions[i]
+      index = i
+  return index
