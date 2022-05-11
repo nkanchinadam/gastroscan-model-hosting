6df045879eb8519cc8b15ac503b756c17072c55c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-05-11 09:31:06 -0400

    added code to receive a tensor, classify it using the appropriate model, and return the prediction to the backend

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from keras.models import load_model
+from determine_label import get_labels, max_probability
 
 app = Flask(__name__)
 
 model = load_model('./models/abnormality_model')
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST"])
 def evaluate():
-  pass
+  if request.method == "POST":
+    tensor = request.json['tensor']
+    modelType = request.json['modelType']
+    
+    labels = None
+    if modelType == 'abnormality':
+      labels = get_labels('./labels/abnormality_labels.txt')
+    else:
+      labels = get_labels('./labels/condition_labels.txt')
+
+    predictions = model.predict(tensor)
+    index = max_probability(predictions)
+    return jsonify(prediction=labels[index])
 
 if __name__ == '__main__':
   app.debug = True
